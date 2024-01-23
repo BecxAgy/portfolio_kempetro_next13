79f4dd3b70f03378feb7e04f5f835bfcae277e8a
rebeca <beca.cout@gmail.com>
2024-01-23 16:32:40 -0300

    chore: about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,41 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| Sobre Nós" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Nossa equipe</span> de
+        engenheiros especializados adota uma metodologia única para identificar
+        as soluções mais adequadas às suas necessidades. Analisamos cada desafio
+        para entregar desde automações simples até projetos disruptivos e
+        personalizados, garantindo a melhor solução técnica.
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
   </section>
 );
 
